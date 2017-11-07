17dadb39d0efc61b6a433e8955ecb96a20b365ed
Scott Ouellette <scottx611x@gmail.com>
2017-11-07 17:35:03 -0500

    A container needs to be running, for the `.Image` to be picked up

--- build_image.sh
+++ build_image.sh
@@ -4,9 +4,20 @@ set -e
 REPO=scottx611x/chromothripsis-pcawg
 
 export STAMP=`date +"%Y-%m-%d_%H-%M-%S"`
+export SUFFIX=-standalone
+echo "image-$STAMP"
+
+CONTAINER_NAME="container-$STAMP$SUFFIX"
+mkdir "/tmp/$CONTAINER_NAME"
 
 sudo docker login -u $DOCKER_USER -p $DOCKER_PASS
+
 docker pull $REPO # Defaults to "latest", but just speeds up the build, so precise version doesn't matter.
 docker build --cache-from $REPO \
              --tag image-$STAMP \
              .
+docker run --name $CONTAINER_NAME \
+           --detach \
+           -p 3242:3242 \
+           image-$STAMP
+rm -r "/tmp/$CONTAINER_NAME"
