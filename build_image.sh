7b4de597cf78326193cbdacd0b20eb6d53d67060
Scott Ouellette <scottx611x@gmail.com>
2017-11-07 17:17:21 -0500

    Logging in is a good idea

--- build_image.sh
+++ build_image.sh
@@ -5,6 +5,7 @@ REPO=scottx611x/chromothripsis-pcawg
 
 export STAMP=`date +"%Y-%m-%d_%H-%M-%S"`
 
+sudo docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker pull $REPO # Defaults to "latest", but just speeds up the build, so precise version doesn't matter.
 docker build --cache-from $REPO \
              --tag image-$STAMP \
